e8008bb7888d3a36dc026e4272d8d12924ee80e7
kumari1022 <kumarinagadurgak@gmail.com>
2026-05-22 10:51:22 +0530

    Added frontend setup with React and Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+
+    <div className="p-10">
+
+      <h1 className="text-4xl font-bold text-blue-600">
+        AI Code Reviewer
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+        Enterprise AI-powered code analysis platform
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
